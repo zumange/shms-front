bd48e30da26f3cc06b3e012a2e01d496267b48a5
Doknjaš Mihael <doknjas.mihael@gmail.com>
2024-12-06 00:58:53 +0100

    add build and run script

---
+++ run-app-clean.bat
@@ -0,0 +1,14 @@
+@echo off
+git checkout main
+git pull
+
+for /f "tokens=*" %%i in ('docker ps -q --filter "name=shms-front"') do (
+    docker stop %%i
+    docker rm %%i
+)
+
+docker rmi -f shms-front
+
+docker build -t shms-front .
+
+docker run -p 4201:4200 shms-front
